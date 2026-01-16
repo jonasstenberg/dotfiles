a3736fc12501735350c6845c181bf61f5ecb6cb1
Jonas Stenberg <github@stenberg.io>
2026-01-16 11:03:46 +0100

    Switch to kotlin-lsp for Kotlin support
    - Replace kotlin_language_server with kotlin_lsp
    - Add blink.cmp LSP configuration
    - Update mason to install kotlin-lsp

--- neovim/.config/nvim/lua/plugins/kotlin.lua
+++ neovim/.config/nvim/lua/plugins/kotlin.lua
@@ -4,8 +4,20 @@ return {
     opts = {
       servers = {
         -- Disable kotlin_language_server - it's too buggy for monorepos
-        kotlin_language_server = {
-          enabled = false,
+        kotlin_lsp = {},
+      },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lsp = {
+            enabled = true,
+          },
         },
       },
     },
