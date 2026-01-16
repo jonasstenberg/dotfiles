b1ce7aa9a79106c832e3f66b927bfdd9496d85f8
Jonas Stenberg <github@stenberg.io>
2026-01-16 13:32:54 +0100

    Fix vim-tmux-navigator keymaps to override LazyVim defaults

--- neovim/.config/nvim/lua/plugins/tmux-navigator.lua
+++ neovim/.config/nvim/lua/plugins/tmux-navigator.lua
@@ -1,6 +1,19 @@
 return {
   {
     "christoomey/vim-tmux-navigator",
-    lazy = false,
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<cr>" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<cr>" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<cr>" },
+      { "<C-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+    },
   },
 }
