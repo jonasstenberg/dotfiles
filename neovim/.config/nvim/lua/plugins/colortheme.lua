95700457813148f74408e7e9eba5f1d3e5c8ccc0
Jonas Stenberg <github@stenberg.io>
2026-01-16 11:03:39 +0100

    Customize catppuccin line number colors
    Add brighter line numbers and bold yellow current line number

--- neovim/.config/nvim/lua/plugins/colortheme.lua
+++ neovim/.config/nvim/lua/plugins/colortheme.lua
@@ -2,6 +2,14 @@ local M = {
   "catppuccin/nvim",
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      custom_highlights = function(colors)
+        return {
+          LineNr = { fg = colors.overlay1 }, -- Brighter line numbers
+          CursorLineNr = { fg = colors.yellow, style = { "bold" } }, -- Bold yellow current line number
+        }
+      end,
+    })
     vim.cmd.colorscheme("catppuccin-mocha")
   end,
 }
